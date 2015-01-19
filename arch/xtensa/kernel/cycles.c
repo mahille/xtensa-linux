87dbc359c5283f41e84a1635d1c12dbc9add440f
Nils Asmussen <nils@script-solution.de>
2015-01-19 16:36:37 +0100

    Moved get_cycles at one place and removed unused code.

--- arch/xtensa/kernel/cycles.c
+++ arch/xtensa/kernel/cycles.c
@@ -1,15 +1,6 @@
 #include <linux/cycles.h>
 #include <asm/uaccess.h>
 
-static unsigned _get_cycles(void) {
-	unsigned val;
-	__asm__ volatile (
-		"rsr    %0, CCOUNT;"
-		: "=a" (val) : : "memory"
-	);
-	return val;
-}
-
 asmlinkage long sys_get_cycles(unsigned *val) {
 	unsigned cycles = _get_cycles();
 	put_user(cycles, val);
--- include/linux/cycles.h
+++ include/linux/cycles.h
@@ -3,6 +3,15 @@
 
 #include <linux/linkage.h>
 
+static unsigned _get_cycles(void) {
+    unsigned val;
+    __asm__ volatile (
+        "rsr    %0, CCOUNT;"
+        : "=a" (val) : : "memory"
+    );
+    return val;
+}
+
 asmlinkage long sys_get_cycles(unsigned *val);
 
 #endif
